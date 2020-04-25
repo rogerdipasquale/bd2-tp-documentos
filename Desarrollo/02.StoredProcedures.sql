347159fe5bb1ef38ab065a2cc24be805f921103c
Javier <jmartinez@identicum.com>
2020-04-25 20:51:27 -0300

    modificacion SP revisar documento

--- Desarrollo/02.StoredProcedures.sql
+++ Desarrollo/02.StoredProcedures.sql
@@ -1,31 +1,40 @@
 --Revisar Documento
 CREATE PROCEDURE revisarDocumento
-	@id_doc int,
-	@revisor int
+    @id_doc int,
+    @revisor int
 AS
+IF(SELECT v.aprobador
+FROM VERSION v
+WHERE id_documento = @id_doc )IS NOT NULL 
+RETURN 
+ELSE
 BEGIN
-	UPDATE VERSION
+    UPDATE VERSION
 	SET revisor = @revisor
-	WHERE id_documento = @id_doc 
+	WHERE id_documento = @id_doc
 END
 
 --Aprobar Documento
 CREATE PROCEDURE aprobarDocumento
-	@id_doc int,
-	@fecha_pub datetime,
-	@aprobador int,
-	@version char(8)
+    @id_doc int,
+    @fecha_pub datetime,
+    @aprobador int,
+    @version char(8)
 AS
-IF (SELECT v.revisor FROM VERSION v WHERE id_documento = @id_doc) IS NULL
+IF (SELECT v.revisor
+FROM VERSION v
+WHERE id_documento = @id_doc) IS NULL
 RETURN
-IF (select v.aprobador FROM VERSION v WHERE id_documento =@id_doc) IS NOT NULL 
+IF (select v.aprobador
+FROM VERSION v
+WHERE id_documento =@id_doc) IS NOT NULL 
 RETURN
 ELSE
 BEGIN
-	UPDATE VERSION
+    UPDATE VERSION
 	SET fecha_publicacion = @fecha_pub,borrador = 0,aprobador = @aprobador,version = @version
-	WHERE id_documento = @id_doc 
-	UPDATE DOCUMENTO 
+	WHERE id_documento = @id_doc
+    UPDATE DOCUMENTO 
 	SET version_actual = @version,vencimiento = DATEADD(MONTH,6,GETDATE())
-	WHERE id = @id_doc 
+	WHERE id = @id_doc
 END
