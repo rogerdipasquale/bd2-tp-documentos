e6a0112a8da8b30c17aeaa5d34cf7782575fcf45
Javier <jmartinez@identicum.com>
2020-04-25 20:47:59 -0300

    modificaciones stored procedure aprobar documento

--- Desarrollo/02.StoredProcedures.sql
+++ Desarrollo/02.StoredProcedures.sql
@@ -1,22 +1,31 @@
 --Revisar Documento
 CREATE PROCEDURE revisarDocumento
-    @id_doc int,
-    @revisor int
+	@id_doc int,
+	@revisor int
 AS
 BEGIN
-    UPDATE VERSION
+	UPDATE VERSION
 	SET revisor = @revisor
-	WHERE id_documento = @id_doc
+	WHERE id_documento = @id_doc 
 END
 
 --Aprobar Documento
 CREATE PROCEDURE aprobarDocumento
-    @id_doc int,
-    @fecha_pub datetime,
-    @aprobador int
+	@id_doc int,
+	@fecha_pub datetime,
+	@aprobador int,
+	@version char(8)
 AS
+IF (SELECT v.revisor FROM VERSION v WHERE id_documento = @id_doc) IS NULL
+RETURN
+IF (select v.aprobador FROM VERSION v WHERE id_documento =@id_doc) IS NOT NULL 
+RETURN
+ELSE
 BEGIN
-    UPDATE VERSION
-	SET fecha_publicacion = @fecha_pub,borrador = 0,aprobador = @aprobador
-	WHERE id_documento = @id_doc
+	UPDATE VERSION
+	SET fecha_publicacion = @fecha_pub,borrador = 0,aprobador = @aprobador,version = @version
+	WHERE id_documento = @id_doc 
+	UPDATE DOCUMENTO 
+	SET version_actual = @version,vencimiento = DATEADD(MONTH,6,GETDATE())
+	WHERE id = @id_doc 
 END
