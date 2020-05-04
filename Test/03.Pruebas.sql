e09fada322e5a097c71abbb0502b2df39ae76adb
Javier <jmartinez@identicum.com>
2020-05-04 19:35:49 -0300

    agregado de casos de prueba

--- Test/03.Pruebas.sql
+++ Test/03.Pruebas.sql
@@ -4,6 +4,9 @@ INSERT INTO DOCUMENTO
 VALUES
     ('Documento 1', 'a', 'borrador', '2020-05-22 00:00:00')
 
+SELECT *
+FROM DOCUMENTO
+
 --Inserción de Participantes
 INSERT INTO PARTICIPANTE
     (id,id_usuario_ldap,nombre)
@@ -17,12 +20,18 @@ INSERT INTO PARTICIPANTE
     (id,id_usuario_ldap,nombre)
 VALUES(3, 3, 'Usuario Creador')
 
---Inserción de Versión inicial del 
+SELECT *
+FROM PARTICIPANTE
+
+--Inserción de Versión inicial de documento
 INSERT INTO VERSION
     (id_documento,version,fecha_creacion,borrador,referente)
 SELECT d.id, d.version_actual, GETDATE(), 1, p.id
 FROM DOCUMENTO d, PARTICIPANTE p
 WHERE p.id = 3
+
+SELECT *
+FROM DOCUMENTO
 
 --Ejecución de Stored Procedure Revisar Documento
 DECLARE @id_d int
@@ -32,6 +41,17 @@ SET @rev = 1
 EXEC revisarDocumento
 @id_doc = @id_d,
 @revisor = @rev
+
+DECLARE @id_d int
+DECLARE @rev int
+SET @id_d = 4
+SET @rev = 2
+EXEC revisarDocumento
+@id_doc = @id_d,
+@revisor = @rev
+
+SELECT *
+FROM VERSION
 
 --Ejecución de Stored Procedure Aprobar Documento
 DECLARE @id_d int
@@ -48,6 +68,23 @@ EXEC aprobarDocumento
 @aprobador = @apr,
 @version = @ver
 
+DECLARE @id_d int
+DECLARE @fec_p datetime
+DECLARE @apr int
+DECLARE @ver char(8)
+SET @id_d = 4
+SET @fec_p = GETDATE()
+SET @apr = 3
+SET @ver = 'final'
+EXEC aprobarDocumento
+@id_doc = @id_d,
+@fecha_pub = @fec_p,
+@aprobador = @apr,
+@version = @ver
+
+SELECT *
+FROM VERSION
+
 --Ejecucion de Stored Procedure Crear Documento
 DECLARE @tit varchar(250)
 DECLARE @tip char(1)
@@ -59,6 +96,33 @@ EXEC crear_documento
 @titulo = @tit,
 @tipo = @tip,
 @referente = @refe
+
+DECLARE @tit varchar(250)
+DECLARE @tip char(1)
+DECLARE @refe int
+SET @tit = 'Doc Prueba'
+SET @tip = 'c'
+SET @refe = 1
+EXEC crear_documento
+@titulo = @tit,
+@tipo = @tip,
+@referente = @refe
+
+DECLARE @tit varchar(250)
+DECLARE @tip char(1)
+DECLARE @refe int
+SET @tit = 'Doc Prueba2'
+SET @tip = 'a'
+SET @refe = 3
+EXEC crear_documento
+@titulo = @tit,
+@tipo = @tip,
+@referente = @refe
+
+SELECT *
+FROM DOCUMENTO
+SELECT *
+FROM VERSION
 
 --Ejecución de Stored Procedure Asociar Documento
 DECLARE @id_o int
@@ -105,6 +169,8 @@ EXEC asociar_documento
 @documento_destino = @id_d,
 @tipo_relacion = @tipo_r
 
+SELECT *
+FROM RELACION
 
 --Ejecucion Stored Procedure Eliminar Participante
 DECLARE @refe int
@@ -121,6 +187,9 @@ DECLARE @refe int
 SET @refe = 3
 EXEC eliminar_participante
 @id = @refe
+
+SELECT *
+FROM PARTICIPANTE
 
 --Stored Procedure futuros vencimientos
 EXEC informe_vencimientos
